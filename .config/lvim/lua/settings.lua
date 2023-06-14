abea5be8e06ed2968099d761fdefd95bcc8b6638
Harun Kilic <haki@nrgi.dk>
2023-06-14 18:20:58 +0200

    update

---
+++ .config/lvim/lua/settings.lua
@@ -0,0 +1,11 @@
+
+-----------------------------------------------------------
+-- LunarVim settings
+--- General LunarVim settings
+-----------------------------------------------------------
+-----------------------------------------------------------
+-- LunarVim Theme
+-----------------------------------------------------------
+-- lvim.colorscheme = "tokyonight"
+-- vim.g.tokyonight_style = "storm"
+lvim.colorscheme = "gruvbox"
